2111d1a7ebf6f3613ebadd185e63057617ef4a01
Krut Lee <ledzepplin001@gmail.com>
2021-11-06 21:56:50 +0800

    added new pytyhon ai file

---
+++ pycharm.py
@@ -0,0 +1,69 @@
+def count(n):
+    if n == 1:
+        return 1
+    return n + count(n - 1)
+
+def mouse(n):
+    if n == 1:
+        return 1
+    return n + mouse(n - 1)
+
+def cat(n):
+    if n == 1:
+        return 1   
+    return n + cat(n - 1)
+
+def dog(n):
+    if n == 1:
+
+        return 1    
+    return n + dog(n - 1)
+
+def rabbit(n):
+    if n == 1:
+        return 1
+    return n + rabbit(n - 1)
+
+def horse(n):   
+    if n == 1:
+        return 1
+    return n + horse(n - 1)
+
+def sheep(n):
+    if n == 1:
+        return 1
+    return n + sheep(n - 1)
+
+
+def main():
+    print(count(5))
+    print(mouse(5))
+    print(cat(5))
+    print(dog(5))
+    print(rabbit(5))
+    print(horse(5))
+    print(sheep(5))     
+
+def main2():
+    print(count(5))
+
+def main3():
+    print(count(5))
+
+def main4():
+    print(count(5))
+
+def main5():
+    print(count(5))
+
+def main6():
+    print(count(5))
+
+
+def count(n):
+    if n == 1:
+        return 1
+    return n + count(n - 1)
+
+
+# Path: pycharm.py
